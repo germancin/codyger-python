9cc1ffb61b569a0a93b4adcc66fc0db303be9633
german gonzalez <gcomlnk@gmail.com>
2018-05-22 17:36:43 -0400

    adding default values to model

--- djorg2/bookmark/models.py
+++ djorg2/bookmark/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
 
 class Bookmarks(models.Model):
 
-    notes = models.TextField(blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    url = models.URLField('URL', unique=True, default='')
+    name = models.CharField(max_length=200, default='')
+    notes = models.TextField(blank=True, default='')
