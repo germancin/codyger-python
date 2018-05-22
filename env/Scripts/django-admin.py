f17b70358ff2d71b8bc847bfb6553c77cef2d642
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-05-22 18:14:01 -0400

    Added admin
    germancody

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\knoxv\desktop\lambda\(20)dj~1\codyge~1\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
