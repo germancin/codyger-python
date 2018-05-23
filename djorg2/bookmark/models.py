188020be5b64bb242c7596339f5b55a2eb8fb46d
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-05-23 18:57:06 -0400

    Bookmarks View

--- djorg2/bookmark/models.py
+++ djorg2/bookmark/models.py
@@ -4,7 +4,6 @@ from uuid import uuid4
 # Create your models here.
 
 class Bookmarks(models.Model):
-
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     url = models.URLField('URL', unique=True, default='')
     name = models.CharField(max_length=200, default='')
--- djorg2/bookmark/views.py
+++ djorg2/bookmark/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Bookmarks
+
 # Create your views here.
+def index(request):
+    context = {
+        'bookmarks': Bookmarks.objects.all(),
+    }
+    return render(request, 'bookmarks/index.html', context)
